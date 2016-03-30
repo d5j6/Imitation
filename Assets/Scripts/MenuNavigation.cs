bc216a5c841775626e70fb27423d71c6d8dce3e3
Tim P <tperry0126@gmail.com>
2016-03-30 12:51:13 -0700

    first commit

---
+++ Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuNavigation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame(){
+		Application.LoadLevel("Character Selector");
+	}
+
+	public void LoadLevel(){
+		Application.LoadLevel ("Load Menu");
+	}
+
+	public void Options(){
+		Application.LoadLevel ("Options");
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setPlayer(GameObject obj){
+		player = obj;
+	}
+}
